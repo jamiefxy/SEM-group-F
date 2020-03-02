eeb910afaa50f2ac19e3012f169c096fef303b4e
Alexander Nicholls <alexandernicholls@hotmail.com>
2020-03-02 14:39:08 +0000

    Added a code snippet to load all scenes into a list of strings for level progression

--- SEM-lab1/Assets/Scripts/GameController.cs
+++ SEM-lab1/Assets/Scripts/GameController.cs
@@ -9,17 +9,24 @@ public class GameController : MonoBehaviour
     public Text Score;
     float _score = 0.0f, _timer = 5.0f;
     int _strokeCount = 0;
+    string[] _levels;
+    int _currentLevel = 0;
     #endregion
 
-    #region private function
+    #region private functions
     void Start()
     {
         Score.text = $"Score: {_score}\nTimer: {_timer}\nStroke: {_strokeCount}";
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        _levels = new string[sceneCount];
+        for(int i = 0; i < sceneCount; i++)
+        {
+            _levels[i] = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+        }
     }
 
     void Update()
     {
-        
         _timer -= Time.deltaTime;
         Score.text = $"Score: {_score}\nTimer: {(int)_timer}\nStroke: {_strokeCount}";
     }
@@ -35,6 +42,12 @@ public class GameController : MonoBehaviour
         _score += 1;
         _timer = 6.0f;
     }
+
+    void EndGame()
+    {
+        _score = 0;
+        _timer = 0.0f;
+    }
     #endregion
 
     #region public functions
@@ -46,7 +59,8 @@ public class GameController : MonoBehaviour
 
     public void GoalReached()
     {
-        // stub
+        // Log current score and start the next level
+        // If the final level is completed, call End()
     }
     #endregion
 }
