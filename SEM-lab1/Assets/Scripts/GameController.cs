7519f3a83ad3227f23b035d5dc4a7e926f77bbbc
Alexander Nicholls <alexandernicholls@hotmail.com>
2020-03-02 15:54:16 +0000

    Created a simple Menu, needs button functionality

--- SEM-lab1/Assets/Scripts/GameController.cs
+++ SEM-lab1/Assets/Scripts/GameController.cs
@@ -6,22 +6,42 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     #region private variables
-    public Text Score;
     float _score = 0.0f, _timer = 5.0f;
     int _strokeCount = 0;
+    bool playing = false;
     #endregion
 
-    #region private function
+    #region public variables
+    public Text Score;
+    public GameObject menu;
+    public GameObject directionalIndicator;
+    public Button start;
+    public Button help;
+    #endregion
+
+    #region private functions
     void Start()
     {
         Score.text = $"Score: {_score}\nTimer: {_timer}\nStroke: {_strokeCount}";
+
+        start.onClick.AddListener(StartGame);
+        help.onClick.AddListener(StartGame);
+
+        PlayerController playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        Score.enabled = false;
+        menu.SetActive(true);
+        directionalIndicator.SetActive(false);
+        playerController.enabled = false;
     }
 
     void Update()
     {
-        
-        _timer -= Time.deltaTime;
-        Score.text = $"Score: {_score}\nTimer: {(int)_timer}\nStroke: {_strokeCount}";
+        if(playing)
+        {
+            _timer -= Time.deltaTime;
+            Score.text = $"Score: {_score}\nTimer: {(int)_timer}\nStroke: {_strokeCount}";
+        }
     }
 
     // Reset the stroke count to 0
@@ -38,6 +58,13 @@ public class GameController : MonoBehaviour
     #endregion
 
     #region public functions
+
+    public void StartGame()
+    {
+        // Hide the Menu UI and re-enable the PlayerController script, the Directional Indicator and the Score text field
+
+    }
+
     // Increase stroke count by 1
     public void IncrementStrokeCount()
     {
