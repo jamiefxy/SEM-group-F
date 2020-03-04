fb4d3dd8cbd1ff480d62625c043c604ad450141e
Alexander Nicholls <alexandernicholls@hotmail.com>
2020-03-04 21:22:54 +0000

    Solving conflicts in GameController.cs with dev

--- SEM-lab1/Assets/Scripts/GameController.cs
+++ SEM-lab1/Assets/Scripts/GameController.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     #region private variables
-    float _score = 0.0f, _timer = 5.0f;
+    float _score = 0.0f, _timer = 20.0f;
     int _strokeCount = 0;
+    string[] _levels;
     bool playing = false;
     PlayerController playerController;
     #endregion
@@ -27,7 +28,15 @@ public class GameController : MonoBehaviour
     #region private functions
     void Start()
     {
-        Score.text = $"Score: {_score}\nTimer: {_timer}\nStroke: {_strokeCount}";
+        Score.text = $"Stroke: {_strokeCount}";
+
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        Debug.Log($"Scene Count: {sceneCount}");
+        _levels = new string[sceneCount];
+        for(int i = 0; i < sceneCount; i++)
+        {
+            _levels[i] = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+        }
 
         start.onClick.AddListener(StartGame);
         help.onClick.AddListener(Help);
@@ -50,16 +59,11 @@ public class GameController : MonoBehaviour
         }
     }
 
-    // Reset the stroke count to 0
-    void ResetStrokeCount()
+    void EndGame()
     {
-        _strokeCount = 0;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        _score += 1;
-        _timer = 6.0f;
+        GameStats.CurrentLevel = 0;
+        GameStats.Score = 0;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GameStats.CurrentLevel);
     }
     #endregion
 
@@ -101,7 +105,19 @@ public class GameController : MonoBehaviour
 
     public void GoalReached()
     {
-        // stub
+        // Log current score and start the next level
+        // If the final level is completed, call End()
+
+        GameStats.Score += _score;
+        GameStats.Stroke += _strokeCount;
+        if(GameStats.CurrentLevel >= UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings - 1)
+        {
+            EndGame();
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(++GameStats.CurrentLevel);
+        }
     }
     #endregion
 }
